cfb03c7c85493b000f42b11232337710556eb85e
Daniel Spaude <daniel@spaude.de>
2014-09-05 15:18:39 +0200

    WIP: login form

--- app/controllers/signin.js
+++ app/controllers/signin.js
@@ -1,2 +1,21 @@
-module.exports = function($scope) {
-};
+module.exports = ['$scope', '$http', function($scope, $http) {
+
+  $scope.username = "demo";
+  $scope.password = "testing";
+
+  $scope.signinSubmit = function(){
+    var payload = {
+      username: $scope.username,
+      password: $scope.password,
+      grant_type: 'password',
+      client_id: 'test_id',
+      client_secret: 'test_secret'
+    };
+    $http.post("http://localhost:8000/oauth/access_token", payload)
+    .success(function(data){
+      localStorage.setItem('access_token', data.access_token);
+      $rootScope.$broadcast('event:authenticated');
+    });
+
+  };
+}];
