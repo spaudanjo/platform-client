89ca3b8ca5fc42fd747d29cdc9e1150d977d21f1
Daniel Spaude <daniel@spaude.de>
2014-09-05 20:24:47 +0200

    WIP: adding authentication angular service (adding observer pattern so that clients of the Authentication server can be notified when the signin status change)

--- app/controllers/posts.js
+++ app/controllers/posts.js
@@ -1,3 +1,7 @@
-module.exports = function($scope) {
+module.exports = ['$scope', 'Authentication', function($scope, Authentication) {
     $scope.title = 'Posts';
-};
+    var onSigninStatusChanged = function(){
+      debugger;
+    };
+    Authentication.registerObserverCallback(onSigninStatusChanged);
+}];
--- app/services/authentication.js
+++ app/services/authentication.js
@@ -2,7 +2,22 @@ module.exports = function(app){
   app.factory("Authentication", ['$http', function($http){
     var accessToken = "";
     var signinStatus = false;
+
+    // instead of using Angular's $watch
+    // (which could results in performance issues when not used carefully),
+    // we are using the good old observer pattern to inform other actors
+    // when the signinStatus has changed
+    var observerCallbacks = [];
+    var notifyObservers = function(){
+      angular.forEach(observerCallbacks, function(callback){
+        callback();
+      });
+    };
+
     return {
+      registerObserverCallback: function(callback){
+        observerCallbacks.push(callback);
+      },
       signin: function(username, password)
       {
         var payload = {
@@ -16,6 +31,7 @@ module.exports = function(app){
         .success(function(data){
           accessToken = data.access_token;
           localStorage.setItem('access_token', data.access_token);
+          notifyObservers();
         });
       },
       getAccessToken: function(){
@@ -24,5 +40,7 @@ module.exports = function(app){
       getSigninStatus: function(){
         return signinStatus;
       }
+    };
+    
   }]);
 };
