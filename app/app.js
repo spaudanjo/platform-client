b5866a3f86a4d3648af0431e668602c951f9688a
Daniel Spaude <daniel@spaude.de>
2014-09-05 22:05:04 +0200

    add angular-resource

--- app/app.js
+++ app/app.js
@@ -1,9 +1,10 @@
 require('angular/angular');
 require('angular-route/angular-route');
+require('angular-resource/angular-resource');
 require('./modules/ui-tabs');
 require('angular-ui-bootstrap/src/dropdown/dropdown');
 
-var app = angular.module('app', ['ngRoute', 'ui.bootstrap.dropdown', 'ui.tabs'])
+var app = angular.module('app', ['ngRoute', 'ngResource', 'ui.bootstrap.dropdown', 'ui.tabs'])
     .config(require('./routes'))
     .service('Authentication', require('./services/authentication.js'))
     .run(require('./global-event-handlers.js'))
