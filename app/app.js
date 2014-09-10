3f312b7c1d5abfadd525624ade4b1e7dbbf3750f
Daniel Spaude <daniel@spaude.de>
2014-09-10 22:22:15 +0200

    First version of OAuth2 signin/signout feature
    Summary:
    * add simple sign in form and use OAuth2
      * use a http interecptor for catching unauthorized http responses
      * add a ng service for the authentication process
    * signin/signout button (as a ng directive)
    * assumes that there is a dummy backend (configure the backend url in app.js) which allows
      * to sign in with OAuth2 Resource Owner Credentials Flow
      * to fetch posts (only when signed in / with a valid access_token)
    
    Test Plan:
    * try to open /posts without beeing signed in
    * then sign in and try again
    
    Reviewers: rjmackay, seth, shadowhand
    
    Differential Revision: https://phabricator.ushahidi.com/D323

--- app/app.js
+++ app/app.js
@@ -10,6 +10,10 @@ angular.module('app', ['ngRoute', 'ngResource', 'ui.bootstrap.dropdown', 'ui.tab
   .constant('BACKEND_URL', backendUrl)
   .constant('API_URL', backendUrl + '/api/v2')
   .directive('postPreview', require('./directives/post-preview.js'))
+  .directive('signinSignoutButton', require('./directives/signin-signout-button.js'))
+  .service('Authentication', require('./services/authentication.js'))
   .service('PostData', require('./services/data/post.js'))
   .controller('navigation', require('./controllers/navigation.js'))
-  .config(require('./routes'));
+  .config(require('./interceptors/authentication.js'))
+  .config(require('./routes'))
+  .run(require('./global-event-handlers.js'));
