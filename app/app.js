1f559a516a4fe9114d0e174b2f4efe90dc9e61a9
Daniel Spaude <daniel@spaude.de>
2014-09-15 17:32:38 +0200

    Add dummy Posts resource and home page
    Summary:
    * Posts
      * Add angular-resource
      * Add simple data service for posts
      * Remove the inline HTML dummy post data
      * Add a ng directive for single post item preview in the list view
      * introduce API_URL constant for backend url
    * add a simple home page
    * change routes correspondingly ('/' -> homepage, '/posts' -> posts)
    * change require style in app.js
    
    Test Plan: Just checkout the /posts url
    
    Reviewers: seth, shadowhand, rjmackay
    
    Subscribers: rjmackay
    
    Differential Revision: https://phabricator.ushahidi.com/D321

--- app/app.js
+++ app/app.js
@@ -1,9 +1,15 @@
 require('angular/angular');
 require('angular-route/angular-route');
+require('angular-resource/angular-resource');
 require('./modules/ui-tabs');
 require('angular-ui-bootstrap/src/dropdown/dropdown');
 
-var app = angular.module('app', ['ngRoute', 'ui.bootstrap.dropdown', 'ui.tabs'])
-    .config(require('./routes'));
+var backendUrl = 'http://localhost:8000';
 
-require('./controllers/navigation.js')(app);
+angular.module('app', ['ngRoute', 'ngResource', 'ui.bootstrap.dropdown', 'ui.tabs'])
+  .constant('BACKEND_URL', backendUrl)
+  .constant('API_URL', backendUrl + '/api/v2')
+  .directive('postPreview', require('./directives/post-preview.js'))
+  .service('PostData', require('./services/data/post.js'))
+  .controller('navigation', require('./controllers/navigation.js'))
+  .config(require('./routes'));
