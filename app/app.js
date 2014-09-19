68700238aedba8dbc981b56b7b0d38d9a7cb5b38
Daniel Spaude <daniel@spaude.de>
2014-09-19 12:48:55 +0200

    add post resource and configure it for ushahidi backend

--- app/app.js
+++ app/app.js
@@ -1,16 +1,18 @@
 require('angular/angular');
 require('angular-route/angular-route');
+require('angular-resource/angular-resource');
 require('./modules/ui-tabs');
 require('angular-ui-bootstrap/src/dropdown/dropdown');
 
 var backendUrl = 'http://lamu';
 
-angular.module('app', ['ngRoute', 'ui.bootstrap.dropdown', 'ui.tabs'])
+angular.module('app', ['ngRoute', 'ngResource', 'ui.bootstrap.dropdown', 'ui.tabs'])
   .constant('BACKEND_URL', backendUrl)
   .constant('API_URL', backendUrl + '/api/v2')
   .directive('postPreview', require('./directives/post-preview.js'))
   .directive('signinSignoutButton', require('./directives/signin-signout-button.js'))
   .service('Authentication', require('./services/authentication.js'))
+  .service('PostData', require('./services/data/post.js'))
   .controller('navigation', require('./controllers/navigation.js'))
   .config(require('./interceptors/authentication.js'))
   .config(require('./routes'))
