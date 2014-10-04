ae277500a29ed115f80ca898613af45eb55f8ae0
Daniel Spaude <daniel@spaude.de>
2014-10-04 21:42:43 +0200

    WIP: add ui-router

--- app/routes.js
+++ app/routes.js
@@ -10,44 +10,34 @@ module.exports = ['$stateProvider', '$urlRouterProvider', '$locationProvider', f
             controller: require('./controllers/posts.js'),
             templateUrl: 'templates/posts.html'
         })
-        .state('posts', {
-            url: '/posts',
-            controller: require('./controllers/posts.js'),
-            templateUrl: 'templates/posts.html'
-        })
         .state('signin', {
             url: '/signin',
             controller: require('./controllers/signin.js'),
             templateUrl: 'templates/signin.html'
         });
-
-
-
-    // var postRouteConfig = {
-    //   controller: require('./controllers/posts.js'),
-    //   templateUrl: 'templates/posts.html'
-    // };
-    // $routeProvider
-    //     .when('/', postRouteConfig)
-    //     .when('/posts', postRouteConfig)
-    //     .when('/signin', {
-    //         controller: require('./controllers/signin.js'),
-    //         templateUrl: 'templates/signin.html'
-    //     })
-    //     .when('/posts/detail', {
-    //         controller: require('./controllers/posts/detail.js'),
-    //         templateUrl: 'templates/posts/detail.html'
-    //     })
-    //     .when('/posts/add-to-set', {
-    //         controller: require('./controllers/sets/add-to-set.js'),
-    //         templateUrl: 'templates/sets/add-to-set.html'
-    //     })
-    //     .when('/settings', {
-    //         controller: require('./controllers/admin/settings.js'),
-    //         templateUrl: 'templates/admin/settings.html'
-    //     })
-    //     .when('/users', {
-    //         controller: require('./controllers/users/users.js'),
-    //         templateUrl: 'templates/users/users.html'
-    //     });
+        .state('posts', {
+            url: '/posts',
+            controller: require('./controllers/posts.js'),
+            templateUrl: 'templates/posts.html'
+        })
+        .state('posts.detail', {
+            url: '/posts/detail',
+            controller: require('./controllers/posts/detail.js'),
+            templateUrl: 'templates/posts/detail.html'
+        })
+        .state('posts.add-to-set', {
+            url: '/posts/add-to-set',
+            controller: require('./controllers/sets/add-to-set.js'),
+            templateUrl: 'templates/sets/add-to-set.html'
+        })
+        .state('settings', {
+            url: '/settings',
+            controller: require('./controllers/admin/settings.js'),
+            templateUrl: 'templates/admin/settings.html'
+        })
+        .state('users', {
+            url: '/users',
+            controller: require('./controllers/users/users.js'),
+            templateUrl: 'templates/users/users.html'
+        });
 }];
