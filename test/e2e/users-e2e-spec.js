8589676de80806c0ef24d2cefc5b1fc3e47eefd6
Daniel Spaude <daniel@spaude.de>
2014-11-30 15:05:33 +0100

    WIP: users profile; e2e spes

--- test/e2e/users-e2e-spec.js
+++ test/e2e/users-e2e-spec.js
@@ -4,16 +4,16 @@ describe('users profile', function(){
     describe('as a signed in user', function(){
 
         beforeEach(function(){
-            // ... sign in stuff configuration to 2e2 mocking
+            // ... sign in stuff configuration for e2e mocking
         });
 
         describe('"Edit profile" link in user menu', function(){
             beforeEach(function(){
                 //... click the user menu link
             });
-            it('should exist');
+            it('should exist', function(){});
             describe('click it', function(){
-                it('should bring you to the "edit user profile" page');
+                it('should bring you to the "edit user profile" page', function(){});
             });
         });
 
@@ -21,7 +21,7 @@ describe('users profile', function(){
             beforeEach(function(){
                 // go to profile page
             });
-            it('should show the users profile data');
+            it('should show the users profile data', function(){});
             describe('edit users profile', function(){
 
                 describe('successfull API call', function(){
@@ -35,8 +35,8 @@ describe('users profile', function(){
                         // make sure that the user data API was called correctly.
                     });
 
-                    it('should notify the user about successfull update process');
-                    it('should stay on the page and show the updated user data');
+                    it('should notify the user about successfull update process', function(){});
+                    it('should stay on the page and show the updated user data', function(){});
                 });
 
                 describe('unsuccessfull API call', function(){
@@ -49,8 +49,8 @@ describe('users profile', function(){
                         // change users data and hit 'save'
                     });
 
-                    it('should notify the user about the unsuccessfull update process');
-                    it('should stay on the page and show the old user data');
+                    it('should notify the user about the unsuccessfull update process', function(){});
+                    it('should stay on the page and show the old user data', function(){});
                 });
 
             });
