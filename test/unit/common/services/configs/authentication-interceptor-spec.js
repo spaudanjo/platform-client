fd65d6bd65342935eb670614c0848568bb3d8610
Daniel Spaude <daniel@spaude.de>
2015-01-09 19:09:48 +0100

    WIP: auth interceptor test

---
+++ test/unit/common/services/configs/authentication-interceptor-spec.js
@@ -0,0 +1,53 @@
+var rootPath = '../../../../../';
+
+describe('authentication interceptor', function(){
+
+    var $httpBackend,
+        $rootScope,
+        $httpProviderIt,
+        BACKEND_URL,
+        mockedSessionData;
+
+    beforeEach(function(){
+        var testApp = angular.module('testApp', [], function($httpProvider){
+            //save our interceptor
+            $httpProviderIt = $httpProvider;
+        });
+
+        mockedSessionData = {};
+        testApp.service('Session', function(){
+            return {
+                clearSessionData: function(){
+                    mockedSessionData = {};
+                },
+                setSessionDataEntries: function(entries){
+                    mockedSessionData = angular.extend({}, mockedSessionData, entries);
+                },
+                getSessionDataEntry: function(key){
+                    return mockedSessionData[key];
+                },
+                setSessionDataEntry: function(key, value){
+                    mockedSessionData[key] = value;
+                }
+            };
+        })
+        .config(require(rootPath+'app/common/configs/authentication-interceptor.js'))
+
+        require(rootPath+'test/unit/simple-test-app-config.js')(testApp);
+
+        angular.mock.module('testApp');
+    });
+
+    beforeEach(inject(function(_$httpBackend_, _$rootScope_){
+        $httpBackend = _$httpBackend_;
+        $rootScope = _$rootScope_;
+    }));
+
+    describe('$httpProvider', function(){
+
+        it('should have the authInterceptor', function () {
+            expect($httpProviderIt.interceptors).toContain('authInterceptor');
+        });
+
+    });
+});
