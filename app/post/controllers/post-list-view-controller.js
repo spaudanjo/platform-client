3c3cf09da325fac81b49f1e58871f0a2ba425cdd
Daniel Spaude <daniel@spaude.de>
2015-01-17 15:21:29 +0100

    WIP: pagination

--- app/post/controllers/post-list-view-controller.js
+++ app/post/controllers/post-list-view-controller.js
@@ -2,11 +2,26 @@ module.exports = ['$scope', '$translate', 'PostEndpoint', function($scope, $tran
 	$translate('post.posts').then(function(postsTranslation){
 		$scope.title = postsTranslation;
 	});
-	PostEndpoint.query().$promise.then(function(posts){
-		$scope.posts = posts;
-	});
 
 	$scope.currentPage = 1;
-	$scope.totalItems = 100;
+	$scope.itemsPerPage = 3;
+	// set a guessed initial value for totalItems
+	// untill we have the correct total_count value from backend request
+	// $scope.totalItems = $scope.itemsPerPage * 2;
+
+	var getPostsForPagination = function(){
+		PostEndpoint.query({
+			offset: ($scope.currentPage - 1) * $scope.itemsPerPage,
+			limit: $scope.itemsPerPage
+		}).$promise.then(function(posts){
+			$scope.posts = posts.results;
+			$scope.totalItems = posts.total_count;
+		});
+	};
+
+	$scope.pageChanged = getPostsForPagination;
+	$scope.itemsPerPageChanged = getPostsForPagination;
+
+	getPostsForPagination();
 
 }];
