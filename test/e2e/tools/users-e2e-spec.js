434b27e6f1880d5c0d3de808ab53b36d26f1a480
Daniel Spaude <daniel@spaude.de>
2015-01-17 19:41:07 +0100

    Revert "WIP: try out http-backend-proxy for e2e tests (mock backend directly in e2e tests)"
    This reverts commit eab61cc48e50d9c34af55a1a91c76fd5131dca25.

--- test/e2e/tools/users-e2e-spec.js
+++ test/e2e/tools/users-e2e-spec.js
@@ -8,45 +8,12 @@ var getLastUrlPart = function(url){
 
 describe('users management', function() {
 
-    var HttpBackend = require('http-backend-proxy');
-    var proxy = new HttpBackend(browser);
-
     describe('as a loggedin admin user', function(){
 
         var userMenuLink;
 
         beforeEach(function() {
-            // proxy.whenGET('/login').respond(200);
-            // proxy.whenGET('/logoff').respond(200);
-
-            // proxy.onLoad.whenGET('http://ushahidi-backend/logoff').respond(200);
-
-            // proxy.whenPOST('/oauth/token').respond(function(method, url, data) {
-            //     var reqPayload = JSON.parse(data);
-            //     if(reqPayload.username === 'admin' && reqPayload.password === 'admin')
-            //     {
-            //         return [200, {
-            //             'access_token':'UmexrkSXVsHeEzGH1TMjYjvX344iB94XZK34nIVw',
-            //             'token_type':'Bearer',
-            //             'expires':2414253574,
-            //             'expires_in':3600,
-            //             'refresh_token':'o1sw8yr6b8BuH00RlIEeLv3v75bzZWZfymquNlKs',
-            //             'refresh_token_expires_in':604800
-            //         }, {}];
-            //     }
-            //     else
-            //     {
-            //         return [400, {
-            //             'error':'invalid_request',
-            //             'error_description':'The user credentials were incorrect.'
-            //         }, {}];
-            //     }
-            // });
-
-
-            // browser.pause();
             browser.get('/login');
-            // browser.pause();
 
             element(by.model('username')).sendKeys('admin');
             element(by.model('password')).sendKeys('admin');
@@ -71,7 +38,7 @@ describe('users management', function() {
 
                 describe('with some existing users in the backend', function(){
                     it('should list the users', function(){
-
+                        
                     });
                 });
 
