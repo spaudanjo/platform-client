454b15aa5fe348b1da8921bb4576cb5ed3c1f1dd
Daniel Spaude <daniel@spaude.de>
2015-02-04 11:09:40 +0100

    WIP: location

--- app/post/directives/post-location-directive.js
+++ app/post/directives/post-location-directive.js
@@ -9,7 +9,22 @@ module.exports = ['leafletData', '$http', function(leafletData, $http){
             mapId: '='
         },
         templateUrl: 'templates/posts/location.html',
-        controller: ['$scope', function($scope) {
+        controller: ['$scope', '$geolocation', function($scope, $geolocation) {
+
+            $scope.getCurrentPosition = function(event){
+                event.preventDefault();
+                $geolocation.getCurrentPosition({
+                    timeout: 60000
+                }).then(function(geoposition){
+                    debugger;
+                });
+            };
+
+            // $geolocation.watchPosition({
+            //     timeout: 60000,
+            //     maximumAge: 250,
+            //     enableHighAccuracy: true
+            // });
 
             var marker = null;
 
@@ -35,14 +50,29 @@ module.exports = ['leafletData', '$http', function(leafletData, $http){
                     }
                 },
 
+                updateLatLon: function(lat, lon){
+                    if($scope.values[$scope.attribute.key] !== null)
+                        $scope.values[$scope.attribute.key] = {};
+                    if($scope.values[$scope.attribute.key][$scope.key] !== null)
+                        $scope.values[$scope.attribute.key][$scope.key] = {};
+
+                    $scope.values[$scope.attribute.key][$scope.key].lat = lat;
+                    $scope.values[$scope.attribute.key][$scope.key].lon = lon;
+                },
+
                 searchLocation: function(event){
+                    var jo = $geolocation;
+                    debugger;
                     event.preventDefault();
+                    var that = this;
                     $http.get('http://nominatim.openstreetmap.org/search?q=' + escape($scope.searchLocationTerm) + '&format=json').success(
                         function(data, status, headers, config){
                             var lat = data[0].lat,
                             lon = data[0].lon;
-                            debugger;
-                            $scope.values['lat'] = lat;
+
+                            that.updateLatLon(lat, lon);
+                            // $scope.values['lat'] = lat;
+
                             leafletData.getMap($scope.attribute.key).then(function(map){
                                 var drawnItems = $scope.controls.edit.featureGroup;
 
